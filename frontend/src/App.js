f3d34bf4aa77fa81e0038cb028aa9e0f186e6388
manchott <manchott@naver.com>
2023-01-05 16:09:58 +0900

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let name = '김철수';
+  return (
+    <div className='App'>
+      안녕하세요, {name} 님.
+    </div>
+  );
+}
+
+export default App;
